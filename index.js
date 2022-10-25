9ce2b8a5b98fc2dfcb10a55e3b940fd3d74ef991
MrMernDeveloper <rakibkst16@gmail.com>
2022-10-25 11:47:42 +0600

    added courses details

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 
+const courses = require('./data/courses.json')
+
+app.get('/courses', (req, res) => {
+    res.send(courses)
+});
 
 app.get('/', (req, res) => {
-    res.send(' News Api Running')
+    res.send(' Courses Api Running')
 });
 
 app.listen(port, () => {
